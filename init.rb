c7d4245143412c2b95a9893a0873573e1dce3353
Steve Valaitis <steve@digitalnothing.com>
2010-11-02 09:08:04 -0500

    Moved init.rb to eliminate deprecation warning

--- init.rb
+++ init.rb
@@ -1,1 +1,7 @@
-require File.dirname(__FILE__) + "/rails/init.rb"
+#require 'permit'
+
+ActiveRecord::Base.send :include,
+  Permit::Models::AuthorizationExtensions,
+  Permit::Models::AuthorizableExtensions,
+  Permit::Models::RoleExtensions,
+  Permit::Models::PersonExtensions
--- rails/init.rb
+++
@@ -1,7 +0,0 @@
-#require 'permit'
-
-ActiveRecord::Base.send :include,
-  Permit::Models::AuthorizationExtensions,
-  Permit::Models::AuthorizableExtensions,
-  Permit::Models::RoleExtensions,
-  Permit::Models::PersonExtensions
